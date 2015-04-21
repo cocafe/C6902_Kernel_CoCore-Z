0d2c46eebfa3647fc7eac17737a5cfa0c211d58a
cocafe <cocafehj@gmail.com>
2015-04-21 17:40:49 +0800

    mdss: Add display state helper
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel_driver.c
+++ drivers/video/msm/mdss/mdss_dsi_panel_driver.c
@@ -25,6 +25,7 @@
 #include <linux/qpnp/pwm.h>
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
+#include <linux/mdss_dsi_panel.h>
 
 #include "mdss_mdp.h"
 #include "mdss_dsi.h"
@@ -82,6 +83,11 @@ static int mdss_dsi_panel_pcc_setup(struct mdss_panel_data *pdata);
 static void vsync_handler(struct mdss_mdp_ctl *ctl, ktime_t t);
 
 struct mdss_mdp_vsync_handler vs_handle;
+
+bool mdss_panel_is_on(void)
+{
+	return display_onoff_state;
+}
 
 /* pcc data infomation */
 #define PANEL_SKIP_ID			0xff
